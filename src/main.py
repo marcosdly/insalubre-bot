1c0d316a0e4f304c72ab5c5ced91f0e63e54c47b
Marcos Delay <dev.marcosdly@gmail.com>
2023-10-23 21:42:53 -0300

    Typo

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
+from os.path import isfile, join
+from shutil import copyfile
+
 from client import client
 from events.on_message import on_message
 from events.on_voice_state_update import on_voice_state_update
 from local.io import ProjectFolders, get_secrets
-from os.path import isfile, join
-from shutil import copyfile
+
 
 @client.event
 async def on_connect():
-    print(f"Bot successfully started and connected!")
+    print("Bot successfully started and connected!")
     print(
         f"Username: {client.user.name}",
         f"ID: {client.user.id}",
