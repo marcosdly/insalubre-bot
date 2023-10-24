e63de6575cd2c0ef5d689f57b8e799abcc13c2ae
Marcos Delay <dev.marcosdly@gmail.com>
2023-10-23 21:44:24 -0300

    Fixed: other actions trigger whatsapp message if you were already in a voice channel

--- src/events/on_voice_state_update.py
+++ src/events/on_voice_state_update.py
@@ -2,14 +2,26 @@ import discord
 from local.io import raw_access_db
 from whatsapp import green_api
 
-async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+
+async def on_voice_state_update(
+    member: discord.Member,
+    before: discord.VoiceState,
+    after: discord.VoiceState
+):
     db = raw_access_db()
     if member.id not in db["whatsapp"]["notify"]:
         return
-    
-    if before.channel is None and after.channel is not None:
+
+    if (
+        (before.channel is not None and after.channel is not None)
+        and before.channel.id == after.channel.id
+    ):
+        return
+
+    if after.channel is not None:
         green_api.sending.sendMessage(
             db["whatsapp"]["targetGroupId"],
-            f"Brother {member.nick if member.nick else member.name} entrou no canal {after.channel.name} no servidor {after.channel.guild.name}."
+            f"Brother {member.nick if member.nick else member.name} entrou no"
+            f"canal {after.channel.name} no servidor {after.channel.guild.name}."
         )
         return
