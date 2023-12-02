8bded2d3d4837d0a211dc0c1a1de2804c9090bbf
Marcos Delay <dev.marcosdly@gmail.com>
2023-12-02 00:40:16 -0300

    Reorganized client definition

--- src/client.py
+++ src/client.py
@@ -7,3 +7,23 @@ intents.message_content = True
 intents.voice_states = True
 
 client = discord.Client(intents=intents)
+
+@client.event
+async def on_connect():
+    print(
+        f"{client.user.name=}",
+        # f"{client.user.id=}",
+        f"{client.user.bot=}",
+        f"{client.user.locale=}",
+        f"{client.user.created_at=}",
+        f"{client.user.display_name=}",
+        f"{client.user.global_name=}",
+        f"{client.user.mfa_enabled=}",
+        f"{client.user.verified=}",
+        f"{client.application.name=}",
+        # f"{client.application.id=}",
+        f"{client.application.owner.name=}",
+        # f"{client.application.owner.id=}",
+        sep="\n"
+    )
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-from constants import Constants
-from local.io import get_secrets
-
 if __name__ == "__main__":
+    # Try not to initialize Constants after logging.
+    # TODO Better logging module definition other than top level logic and variables
+    from constants import Constants
     Constants.initialize()
 
     from client import client
+    from events.on_message import on_message
+    from events.on_voice_state_update import on_voice_state_update
 
+    client.event(on_message)
+    client.event(on_voice_state_update)
+
+    from local.io import get_secrets
     secrets = get_secrets()
+
     client.run(secrets["discordToken"])
