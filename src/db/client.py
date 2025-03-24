166b7b79200b01300fe4bef8c2a8655224acdc3a
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-24 00:38:55 -0300

    fix(db): using filesystem path with URI arg

--- src/db/client.py
+++ src/db/client.py
@@ -7,5 +7,7 @@ if __debug__:
 else:
   db_path = Path('persistent_data.prod.db')
 
-engine = create_async_engine(f'sqlite+aiosqlite://{db_path.absolute()}', echo=__debug__)
+engine = create_async_engine(
+  f'sqlite+aiosqlite:///{db_path.absolute()}', echo=__debug__
+)
 async_session = async_sessionmaker(engine)
