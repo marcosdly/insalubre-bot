fcbcd00681531dfab1eaedcfb8d09c6b6c68aa0b
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-24 11:31:23 -0300

    feat(db): client: check and try inserting hard coded music_bot rows

--- src/db/client.py
+++ src/db/client.py
@@ -1,6 +1,10 @@
+from collections.abc import Sequence
 from pathlib import Path
 
+from sqlalchemy import event, insert, select
+from sqlalchemy.engine.interfaces import DBAPIConnection
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.pool import ConnectionPoolEntry
 
 if __debug__:
   db_path = Path('persistent_data.dev.db')
@@ -11,3 +15,23 @@ engine = create_async_engine(
   f'sqlite+aiosqlite:///{db_path.absolute()}', echo=__debug__
 )
 async_session = async_sessionmaker(engine)
+
+
+@event.listens_for(engine.sync_engine, 'first_connect')
+def ensure_music_bot_data(conn: DBAPIConnection, conn_record: ConnectionPoolEntry):
+  from ...migrations.data_hardcoded.table_music_bot import DATA
+  from .schema import MusicBot
+
+  cursor = conn.cursor()
+  select_stmt = select(MusicBot.user_id)
+  primary_keys: Sequence[int] = cursor.execute(select_stmt)
+  data_intersection = [row for row in DATA if row.user_id not in primary_keys]
+  if len(data_intersection) == 0:
+    return
+
+  insert_stmt = insert(MusicBot).values(data_intersection)
+  cursor.execute(insert_stmt)
+
+  conn.commit()
+  cursor.close()
+  conn.close()
