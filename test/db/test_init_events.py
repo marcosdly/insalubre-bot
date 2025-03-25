6ee7ab1bedfab3be13883753d37c4ecbc6dee8ed
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-25 14:32:21 -0300

    feat(test): test_init_events: add test_create_schema

---
+++ test/db/test_init_events.py
@@ -0,0 +1,46 @@
+from typing import TYPE_CHECKING, Unpack
+
+import pytest
+from sqlalchemy import event
+from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.db.client import dbevent_create_schema
+
+if TYPE_CHECKING:
+  from sqlalchemy.dialects.sqlite.aiosqlite import AsyncAdapt_aiosqlite_connection
+  from sqlalchemy.pool import ConnectionPoolEntry
+
+
+@pytest.fixture(scope='function')
+def async_engine():
+  from sqlalchemy import create_engine
+
+  return create_engine('sqlite+aiosqlite://:memory:')
+
+
+async def test_create_schema(async_engine: AsyncEngine):
+  _called = False
+
+  def run_event(
+    *args: Unpack[tuple['AsyncAdapt_aiosqlite_connection', 'ConnectionPoolEntry']],
+  ):
+    nonlocal _called
+    _called = True
+    dbevent_create_schema(*args)
+
+  event.listen(async_engine.sync_engine, 'first_connect', run_event)
+
+  try:
+    async with async_engine.begin() as _:
+      # event will be called as soon as trying to create connection
+      assert _called, 'event was not called'
+      inspector = Inspector(async_engine.sync_engine)
+      tables_should_exist = ['music_bot']
+
+      # NOTE: metadata.create_all parameter 'checkfirst' is True, so schema creation
+      # will not be forced in case tables already exist.
+      # Pre-runtime tests of schema validation are a matter migration.
+      for table_name in tables_should_exist:
+        assert inspector.has_table(table_name), f"table does not exist: '{table_name}'"
+  except Exception as err:
+    pytest.fail(str(err))
