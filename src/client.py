3be6b0e91b575c99073a1fc247e7a5d04701061f
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-25 13:53:20 -0300

    refactor: remove schema creation from bot.event.on_ready

--- src/client.py
+++ src/client.py
@@ -1,6 +1,5 @@
 from pprint import pprint
 
-import nextcord
 from nextcord.ext import commands
 
 GUILD_IDS: set[int] = set()
@@ -10,14 +9,6 @@ bot = commands.Bot(lazy_load_commands=False)
 
 @bot.event
 async def on_ready():
-  # region INIT DATABASE
-  from src.db.client import engine
-  from src.db.schema import BaseTable
-
-  async with engine.begin() as conn:
-    await conn.run_sync(BaseTable.metadata.create_all)
-  # endregion
-
   for guild in bot.guilds:
     GUILD_IDS.add(guild.id)
   pprint(GUILD_IDS, indent=2, underscore_numbers=True)
