6277d3086ce52b76dc7b0fb4d81e39f29ba6c761
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-25 16:30:29 -0300

    fix(db): client: create schema completely synchonously

--- src/db/client.py
+++ src/db/client.py
@@ -1,17 +1,11 @@
-# from collections.abc import Sequence
 from pathlib import Path
-
-# from sqlalchemy import  insert, select
 from typing import TYPE_CHECKING, Unpack
 
 if TYPE_CHECKING:
   from sqlalchemy.dialects.sqlite.aiosqlite import AsyncAdapt_aiosqlite_connection
   from sqlalchemy.pool import ConnectionPoolEntry
 
-from sqlalchemy.ext.asyncio import (
-  async_sessionmaker,
-  create_async_engine,
-)
+from sqlalchemy.ext.asyncio import create_async_engine
 
 # region Init ORM
 
@@ -20,10 +14,9 @@ if __debug__:
 else:
   db_path = Path('persistent_data.prod.db')
 
-engine = create_async_engine(
+async_engine = create_async_engine(
   f'sqlite+aiosqlite:///{db_path.absolute()}', echo=__debug__
 )
-async_session = async_sessionmaker(engine)
 
 # endregion
 
@@ -34,18 +27,11 @@ async_session = async_sessionmaker(engine)
 def dbevent_create_schema(
   *_: Unpack[tuple['AsyncAdapt_aiosqlite_connection', 'ConnectionPoolEntry']],
 ):
-  import asyncio
+  from src.db.schema import BaseTable
 
-  async def create_schema():
-    from src.db.schema import BaseTable
+  global async_engine
 
-    global engine
-
-    async with engine.begin() as conn:
-      await conn.run_sync(BaseTable.metadata.create_all)
-
-  with asyncio.Runner(debug=__debug__, loop_factory=asyncio.new_event_loop) as runner:
-    runner.run(create_schema())
+  BaseTable.metadata.create_all(async_engine.sync_engine)
 
 
 # endregion
